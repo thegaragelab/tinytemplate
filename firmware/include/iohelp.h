4277baf8ce747457db6a4c9af04dd3c98dcf1d9f
Shane Gough <shane@thegaragelab.com>
2014-05-14 17:44:45 +1000

    Added implementation of hardware PWM helpers.

--- firmware/include/iohelp.h
+++ firmware/include/iohelp.h
@@ -97,24 +97,31 @@ uint16_t adcRead(ANALOG adc, uint8_t skip, uint8_t average);
 // PWM helpers
 //---------------------------------------------------------------------------
 
-/** Initialise a PWM pin
+/** PWM outputs
  *
- * Set up a PWM output pin.
- *
- * @param pin the pin number to initialise.
- * @param frequency the frequency to operate at.
+ * This enumerates the available PWM outputs.
  */
-void pwmInit(uint8_t pin, uint16_t frequency);
+typedef enum _PWM {
+  PWM0 = 0, //!< PWM output 0 (PB0)
+  PWM1 = 1, //!< PWM output 1 (PB1)
+  } PWM;
+
+/** Initialise the PWM system
+ *
+ * Sets up PWM support. The library only supports hardware PWM on TIMER0
+ * so you only get up to two PWM pins - PB0 and PB1.
+ */
+void pwmInit();
 
 /** Set the value of a PWM pin
  *
  * Set the output for a PWM pin.
  *
- * @param pin the pin number of the output to modify.
+ * @param pwm   the PWM output to change
  * @param value the value for the duty cycle ranging from 0 (fully off) to 255
  *              (fully on).
  */
-void pwmOut(uint8_t pin, uint8_t value);
+void pwmOut(PWM pwm, uint8_t value);
 
 //---------------------------------------------------------------------------
 // Software SPI
