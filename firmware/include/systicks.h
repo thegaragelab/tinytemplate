2db013c011f9cff9c25d1e7f91ddaea6067c91bb
Shane Gough <shane@thegaragelab.com>
2014-05-27 11:53:41 +1000

    Fixed TICKS_PER_SECOND to match implementation (@8MHz)

--- firmware/include/systicks.h
+++ firmware/include/systicks.h
@@ -20,7 +20,7 @@ extern "C" {
  *
  * Defines the approximate number of ticks that occur every second.
  */
-#define TICKS_PER_SECOND 64
+#define TICKS_PER_SECOND 60
 
 /** Initialise the 'ticks' subsystem.
  *
